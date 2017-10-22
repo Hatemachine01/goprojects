7f99438ac9f96ad2890582201e4f8d3d15da2b01
Julio <juliodevasquez@gmail.com>
2017-10-21 19:34:27 -0500

    fixed lost variable issue

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -9,6 +9,7 @@ end
 def approve 
 #changes approved? attribute to true
  project = Project.update(params[:id], :approved? => true )
+ @approved_projects = Project.where(approved?:true)
 
  render 'profiles/show'
 end
